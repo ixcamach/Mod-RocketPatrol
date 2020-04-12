d41b8cc03ee7493697025e310aa9ddfa0e75df38
Ixel Camacho <ixcamach@ucsc.edu>
2020-04-11 19:01:54 -0700

    Complete Game

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,6 +5,8 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         scene.add.existing(this);// add to an existng scene, displayList, updateList
         this.isFiring = false;  //track rockets firing stats
+
+        this.sfxRocket = scene.sound.add('sfx_rocket'); //add rocket sfx
     }
 
     update() {
@@ -16,9 +18,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
                 this.x +=2;
             }
         }
-        // fire buttom
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();  //play sfx
         }
         // if fired, move up
         if(this.isFiring && this.y >= 108) {
@@ -30,5 +33,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.y = 431;
         }
 
+    }
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = 431;
     }
 }
--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -9,11 +9,15 @@ class Spaceship extends Phaser.GameObjects.Sprite {
 
     update() {
 // move spacehsip left
-        this.x -= 3;
+        this.x -= game.settings.spaceshipSpeed;
         // wraparound screen bounds
         if(this.x <= 0 - this.width){
             this.x = game.config.width;
         }
 
+    }
+
+    reset() {
+        this.x = game.config.width;
     }
 }
