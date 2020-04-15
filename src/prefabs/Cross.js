618c3646953938b49b2b1674e84f20ea0ccf853e
Ixel Camacho <ixcamach@ucsc.edu>
2020-04-15 16:35:00 -0700

    Updated RocketPatrolMod

--- src/prefabs/Cross.js
+++ src/prefabs/Cross.js
@@ -1,17 +1,17 @@
-//Rocket prefab
-class Rocket extends Phaser.GameObjects.Sprite {
+//Cross prefab
+class Cross extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
         scene.add.existing(this);// add to an existng scene, displayList, updateList
         this.isFiring = false;  //track rockets firing stats
 
-        this.sfxRocket = scene.sound.add('sfx_rocket'); //add rocket sfx
+        this.sfxCross = scene.sound.add('sfx_rocket'); //add rocket sfx
     }
 
     update() {
         // left/right movement
-        if (!this.isFiring) {
+         {
             if(keyLEFT.isDown && this.x >= 47) {
                 this.x -= 2;
             } else if (keyRIGHT.isDown && this.x <= 578) {
@@ -21,7 +21,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         // fire button
         if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
-            this.sfxRocket.play();  //play sfx
+            this.sfxCross.play();  //play sfx
         }
         // if fired, move up
         if(this.isFiring && this.y >= 108) {
--- src/prefabs/Ghost.js
+++ src/prefabs/Ghost.js
@@ -1,5 +1,5 @@
-// Spaceship prefab
-class Spaceship extends Phaser.GameObjects.Sprite {
+// Ghost prefab
+class Ghost extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame);
 
@@ -9,7 +9,7 @@ class Spaceship extends Phaser.GameObjects.Sprite {
 
     update() {
 // move spacehsip left
-        this.x -= game.settings.spaceshipSpeed;
+        this.x -= game.settings.ghostSpeed;
         // wraparound screen bounds
         if(this.x <= 0 - this.width){
             this.x = game.config.width;
