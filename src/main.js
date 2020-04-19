98542f491fa19ce7dfa3384825051930df1977e6
Ixel Camacho <ixcamach@ucsc.edu>
2020-04-19 10:53:00 -0700

    Finish Games: Added Point Breakdown

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+//Ixel Camacho
+//Spooky Booah font by Suby Studio - https://www.dafont.com/spooky-booah.font
+//- Add your own (copyright-free) background music to the Play scene (10)
+//- Create a new scrolling tile sprite for the background (10)
+//- Allow the player to control the Rocket after it's fired (10)
+//- Display the time remaining (in seconds) on the screen (15)
+//- Replace the UI borders with new artwork (15)
+//- Create a new title screen (15)
+//- Create new artwork for all the in-game assets (rocket, spaceships, explosion) (25)
 let config = {
     type: Phaser.CANVAS,
     width: 640,
